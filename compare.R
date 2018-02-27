22941462f766f534f73f23d96e311feee74c12cb
slopp <sean@rstudio.com>
2018-02-26 17:24:37 -0700

    better messages

--- compare.R
+++ compare.R
@@ -1,54 +1,58 @@
-library(dplyr)
-library(tibble)
-
-contribUrl <- function(repo) {
-  paste0(repo, '/src/contrib/')
-}
+suppressMessages({
+  library(dplyr)
+  library(tibble)
+})
 
 
 compare <- function(baseline, new, pkgsToChk) {
   
 # --- First compare available packages
-    avail1 <- available.packages(contribUrl(baseline))
-    avail2 <- available.packages(contribUrl(new))
+    avail1 <- available.packages(contrib.url(baseline, 'source'))
+    avail2 <- available.packages(contrib.url(new, 'source'))
     avail1_t <- as_tibble(avail1) %>% 
-      select(-Repository)
+      select(Package, Version, Imports, Suggests, Depends, LinkingTo)
     avail2_t <- as_tibble(avail2) %>% 
-      select(-Repository)
-    diff <- anti_join(avail2_t, avail1_t)
+      select(Package, Version, Imports, Suggests, Depends, LinkingTo)
+    diff <- suppressMessages({anti_join(avail2_t, avail1_t)})
     if (nrow(diff) > 0 ) {
-      print('Packages from repo2 different from repo1 based on available packages: ')
+      print('repo2 different from repo1. The following repo2 packages are different (based on available packages): ')
       print(diff)
     }
     
 # ---  Second check graph for each package 
     for (p in pkgsToChk) {
       graph1 <- tools::package_dependencies(
-        p,
-        db = avail1,
-        which = 'most',
-        recursive = TRUE
+        p, db = avail1, which = 'most', recursive = TRUE
       )
       names(graph1) <- 'pkg'
+      
       graph2 <- tools::package_dependencies(
-        p,
-        db = avail2,
-        which = 'most',
-        recursive = TRUE
+        p, db = avail2, which = 'most',recursive = TRUE
       )
       names(graph2) <- 'pkg'
       
-      print(sprintf('Missing the following packages from %s in the graph for %s', new, p))
-      print(graph2$pkg[!(graph2$pkg %in% graph1$pkg)])
+      graph2_added <- graph2$pkg[!(graph2$pkg %in% graph1$pkg)]
+      graph2_missing <- graph1$pkg[!(graph1$pkg %in% graph2$pkg)]
+      
+      if (length(graph2_added) > 0) {
+        print(sprintf('%s dependency graph for %s has the following packages that should not be there:', new, p))
+        print(graph2_added)
+      }
+      if (length(graph2_missing) > 0 ) {
+        print(sprintf('%s dependency graph for %s is missing the following packages compared to the baseline: ', new, p))
+        print(graph2_missing)
+      }    
       
     }
     
     
 # --- Third check package install from new
     dir.create('./testlib')
+    print('Attempting to install packages!')
     options(repos = c(CRAN = new))
     for (p in pkgsToChk) {
-      install.packages(p, type = 'source', lib = './testlib') 
+      print(sprintf('Installing %s', p))
+      suppressWarnings({install.packages(p, type = 'source', lib = './testlib', verbose = FALSE, quiet = TRUE)})
     }
     system2('rm','-rf ./testlib')
   
